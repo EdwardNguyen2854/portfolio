76a687207a462b4f8f5cef7b5f062058aae2ccae
Edward <edward.nguyen.2854@gmail.com>
2026-01-05 22:36:36 +0700

    Fix JS ReferenceError causing non-responsive mobile menu (v20)

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     setupProjectFilters();
     setupBlogSearch();
     setupWikiSearch();
-    setupNavHighlight();
     setupMobileMenu();
 });
 
